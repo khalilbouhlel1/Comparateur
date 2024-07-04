807447de359c20b299d8d090838f8a430eb91c0d
khalilbouhlel1 <bouhlel@isikef.u-jendouba.tn>
2024-07-05 00:23:08 +0100

    backend

---
+++ src/main/java/com/Pfe/Comparateur/Models/User_alert.java
@@ -0,0 +1,75 @@
+package com.Pfe.Comparateur.Models;
+
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "users_alert")
+@Data
+public class User_alert {
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Setter
+    @Getter
+    private User user;
+
+    public Product getProducts() {
+        return products;
+    }
+
+    public void setProducts(Product products) {
+        this.products = products;
+    }
+
+    private Product products;
+
+    @Size(max = 20)
+    private double budget;
+
+    private String Price;
+
+
+    public User_alert() {
+    }
+
+    public User_alert(double budget,String Price) {
+        this.budget = budget;
+        this.Price=Price;
+    }
+
+    public  void setProduct(Product products) {
+        this.products=products;
+    }
+
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+//    public String getId() {
+
+}
